94e13868f28d6cb7c0a32e43012ad87312960ebf
Jason Bach <526JBach@frhsd.com>
2025-12-03 14:13:16 -0500

    Create a variable for a title, owner, and a GitHub repository link

--- site.js
+++ site.js
@@ -34,7 +34,9 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
+         title: "IMDB + Jason's Movie Gallery",
+         owner: "Jason",
+         github: "https://github.com/Jason20668?tab=repositories"
       }
     },
       methods: {
